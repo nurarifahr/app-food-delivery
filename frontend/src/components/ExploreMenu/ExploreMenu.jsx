649a2e609b2a837dd717df0602bc9a5212c0f30c
nurarifahr <202030354@mhs.udb.ac.id>
2025-11-22 18:14:08 +0700

    Fix frontend folder (remove submodule)

---
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './ExploreMenu.css'
+import {menu_list} from '../../assets/assets'
+
+const ExploreMenu = ({category, setCategory}) => {
+  return (
+    <div className='explore-container'>
+        <h1>Explore our menu</h1>
+        <p>Choose from a diverse menu featuring a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+        <div className='explore-menu'>
+            {menu_list.map((item, index)=>(
+                <div key={index} 
+                className='explore-list'
+                onClick={()=>setCategory(prev=>prev===item.menu_name?"All" : item.menu_name)}>
+                    <img 
+                    className={category===item.menu_name?"active" : ""}
+                    src={item.menu_image} alt="" />
+                    <p>{item.menu_name}</p>
+                </div>
+            ))}
+        </div>
+        <hr />
+    </div>
+  )
+}
+
+export default ExploreMenu
