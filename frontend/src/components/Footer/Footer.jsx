dfce8c1a104eddc119cf1bafbbd66c87aae87818
nurarifahr <202030354@mhs.udb.ac.id>
2025-11-29 07:48:28 +0700

    footer

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { assets } from '../../assets/assets'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-left">
+            <img src={assets.logo} alt="" />
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur sint modi et, quae facilis delectus quo, obcaecati quibusdam iusto similique ea, sit quidem. Esse sit quos iure laboriosam neque minima modi molestias optio, ipsa commodi corporis officiis alias eos, sunt ea. Quaerat nam, a nobis odit vero consequatur quibusdam animi?</p>
+            <div className="footer-social-icons">
+                <img src={assets.facebook_icon} alt="" />
+                <img src={assets.twitter_icon} alt="" />
+                <img src={assets.linkedin_icon} alt="" />
+            </div>
+        </div>
+        <div className="footer-content-center">
+            <h2>COMPANY</h2>
+            <ul>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+        <div className="footer-content-right">
+            <h2>GET IN TOUCH</h2>
+            <ul>
+                <li>+62-890-456-7890</li>
+                <li>contact@tomato.com</li>
+            </ul>
+        </div>
+      </div>
+      <hr />
+      <p className='footer-copyright'>Copyright 2025 Tomato.com - All Right Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
