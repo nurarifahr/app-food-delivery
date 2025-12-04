61701b1a15b27506d0c9e6f2487d5659cb1d57b5
nurarifahr <202030354@mhs.udb.ac.id>
2025-12-04 23:07:18 +0700

    login popup

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -4,7 +4,7 @@ import {menu_list} from '../../assets/assets'
 
 const ExploreMenu = ({category, setCategory}) => {
   return (
-    <div className='explore-container'>
+    <div className='explore-container' id='explore-menu'>
         <h1>Explore our menu</h1>
         <p>Choose from a diverse menu featuring a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
         <div className='explore-menu'>
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,25 +1,26 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
   const [menu, setMenu] = useState("menu");
   return (
     <div className='navbar'>
-      <img src={assets.logo} alt="" className=''/>
+      <Link to='/'><img src={assets.logo} alt="" className='logo'/></Link>
       <ul className='nav-menu'>
-        <li onClick={()=>setMenu("home")} className={menu==="home" ? "active" : ""}>home</li>
-        <li onClick={()=>setMenu("menu")} className={menu === "menu" ? "active" : ""}>menu</li>
-        <li onClick={()=>setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>mobile-ap</li>
-        <li onClick={()=>setMenu("contact")} className={menu === "contact" ? "active" : ""}>contact us</li>
+        <Link to='/' onClick={()=>setMenu("home")} className={menu==="home" ? "active" : ""}>home</Link>
+        <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu === "menu" ? "active" : ""}>menu</a>
+        <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>mobile-ap</a>
+        <a href='#footer' onClick={()=>setMenu("contact")} className={menu === "contact" ? "active" : ""}>contact us</a>
       </ul>
       <div className='nav-list'>
         <img src={assets.search_icon} alt="" className='icon-search'/>
         <div className='relative'>
             <span className='notif'></span>
-            <img src={assets.basket_icon} alt="" className='icon-basket'/>
+            <Link to='/cart'><img src={assets.basket_icon} alt="" className='icon-basket'/></Link>
         </div>
-        <button>sign in</button>
+        <button onClick={()=>setShowLogin(true)}>sign in</button>
       </div>
     </div>
   )
