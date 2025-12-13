453f9d3d2d4ef2af89e276a615d4a3646ab08435
nurarifahr <202030354@mhs.udb.ac.id>
2025-12-13 14:45:31 +0700

    food-api

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
 
 // app config
 const app = express()
@@ -13,6 +14,10 @@ app.use(cors())
 // db connection
 connectDB();
 
+// api endpoints
+app.use("/api/food",foodRouter)
+app.use("/images",express.static('uploads'))
+
 app.get("/",(req,res)=>{
     res.send("API Working")
 })
@@ -20,5 +25,3 @@ app.get("/",(req,res)=>{
 app.listen(port,()=>{
     console.log(`Server Start on http://localhost:${port}`)
 })
-// mongodb+srv://greatstack:12345567@cluster0.oici9ec.mongodb.net/?
-// mongodb+srv://greatstack:12345567@cluster0.oici9ec.mongodb.net/?appName=Cluster0
